fe5b6637c66c2b5cbe1610b979d809046d387d2e
Juan Imsand <juan.imsand@hotmail.com>
2020-11-15 15:09:03 -0300

    phonebookfullstack

---
+++ phonebookfullstack/build/precache-manifest.8c8f095f57ef03a46b905a0f789dc9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99211fadd38c0619d9d9f2459515cee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8171ed2eaf6e8f71596",
+    "url": "/static/css/main.311a2b5a.chunk.css"
+  },
+  {
+    "revision": "641f0d8728beab65a896",
+    "url": "/static/js/2.b2c77fec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2c77fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8171ed2eaf6e8f71596",
+    "url": "/static/js/main.d501900d.chunk.js"
+  },
+  {
+    "revision": "c8fb754456e431878a99",
+    "url": "/static/js/runtime-main.659ea1bb.js"
+  }
+]);
