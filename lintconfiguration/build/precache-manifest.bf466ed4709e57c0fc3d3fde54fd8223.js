a930b393c777efd3a9a8d3a39a4e6db3f99b0e6c
Juan Imsand <juan.imsand@hotmail.com>
2020-11-18 23:33:05 -0300

    lintconfiguration

---
+++ lintconfiguration/build/precache-manifest.bf466ed4709e57c0fc3d3fde54fd8223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc43a076b67e0c9e8863eded74e7d1f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63db6a11d86806a0e7f9",
+    "url": "/static/css/main.311a2b5a.chunk.css"
+  },
+  {
+    "revision": "641f0d8728beab65a896",
+    "url": "/static/js/2.b2c77fec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2c77fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63db6a11d86806a0e7f9",
+    "url": "/static/js/main.841c5fc5.chunk.js"
+  },
+  {
+    "revision": "c8fb754456e431878a99",
+    "url": "/static/js/runtime-main.659ea1bb.js"
+  }
+]);
